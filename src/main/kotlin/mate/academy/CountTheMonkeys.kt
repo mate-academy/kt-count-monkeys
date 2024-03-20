a5c2635cc64db064c81392e414f7e8dd6a236e88
Keryit <fillimonova@inbox.ru>
2024-03-20 14:08:17 +0100

    solution count

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,5 +3,9 @@ package mate.academy
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    val array = IntArray(count)
+    for (i in 0 until count) {
+        array[i] = i + 1
+    }
+    return array
 }
