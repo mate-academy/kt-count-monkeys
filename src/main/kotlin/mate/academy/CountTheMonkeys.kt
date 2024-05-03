76c1438d43e3b92646dba594f113eb43b8acbd0e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-03 07:37:32 +0300

    CountTheMonkeys.kt

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -1,7 +1,5 @@
 package mate.academy
 
-// Implement the `countTheMonkeys()` function so that it returns an IntArray
-// with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    return if (count > 0) (1..count).toList().toIntArray() else intArrayOf()
 }
