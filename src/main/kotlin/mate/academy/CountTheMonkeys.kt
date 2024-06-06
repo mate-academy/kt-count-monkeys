464d6bde927f6122734eb3439508fe52a1bc9332
Roman Globa <globaroman@gmail.com>
2024-06-06 20:35:16 +0300

    The solution of the task

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -1,7 +1,15 @@
 package mate.academy
 
+const val START_LOOP = 1
+const val INDEX_MINUS = 1
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
-fun countTheMonkeys(count: Int): IntArray {
 
+fun countTheMonkeys(count: Int): IntArray {
+    val arrayMonkeys = IntArray(count)
+    for (i in START_LOOP..count) {
+        arrayMonkeys[i - INDEX_MINUS] = i
+    }
+    return arrayMonkeys
+}
 }
