3076ad4dad650e970aa21a3909beebf103e158db
TonyH277 <gajjduktoxa@gmail.com>
2024-07-04 09:07:47 +0300

    implement countTheMonkeys fun

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -1,7 +1,15 @@
 package mate.academy
 
+private const val ONE = 1
+
+private const val TWO = 2
+
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    val arr = IntArray(count) { ONE }
+    for (i in TWO..count) {
+        arr[i - ONE] = i
+    }
+    return arr
 }
