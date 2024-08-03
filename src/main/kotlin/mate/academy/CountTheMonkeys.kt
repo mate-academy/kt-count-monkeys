ba6f2aca48fc346628710ce27c4e8d2cedeb2507
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-08-03 15:21:30 +0300

    added code countTheMonkeys()

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,5 +3,9 @@ package mate.academy
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    return if (count == 0) {
+        IntArray(0)
+    } else {
+        IntArray(count) { it + 1 }
+    }
 }
