730a54078e2dd8b8a960bf51c36484a271bf5efd
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-20 18:08:25 +0300

    Implemented the feature

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -1,7 +1,19 @@
 package mate.academy
 
+private const val ZERO = 0
+
+private const val ONE = 1
+
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
+    val arrayOfIntegers: IntArray = IntArray(count)
 
+    var index = ZERO;
+    for (i in ONE..count) {
+        arrayOfIntegers[index] = i
+        index++
+    }
+
+    return arrayOfIntegers
 }
