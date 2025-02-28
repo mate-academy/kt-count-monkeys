223a966867dac62db54c7349f5e6df1c0d27be4d
OD <sdenisenko055@gmail.com>
2025-02-28 18:29:18 +0200

    result

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,5 +3,12 @@ package mate.academy
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    if (count == 0) {
+        return intArrayOf()
+    }
+    var monkeys: IntArray = IntArray(count) {0}
+    for (i in 0 until count) {
+        monkeys[i] = i + 1
+    }
+    return monkeys
 }
